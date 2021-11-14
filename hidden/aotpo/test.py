8e6ea499d190b340fbca1d1741b206349bd451ef
Arnold <arnoldtingsu@gmail.com>
2021-11-14 08:52:21 -0800

    MET API QUERY

--- hidden/aotpo/test.py
+++ hidden/aotpo/test.py
@@ -1,12 +1,21 @@
 import requests
+from PIL import Image
 response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/70110")
 art = response.json()
 
 image = art['primaryImage']
+name = art['objectName']
+title = art['title']
+date = art['objectDate']
 
 print(response)
+
 print(art)
 print(image)
+print(name)
+print(title)
+print(date)
+print()
 
 
 img=image
